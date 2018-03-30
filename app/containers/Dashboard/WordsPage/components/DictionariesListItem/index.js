680aed39d5f9a81b4a45c0c2dbb5c6e62d77bba3
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-03-30 16:10:12 +0200

    Show words count in the dictionary

--- app/containers/Dashboard/WordsPage/components/DictionariesListItem/index.js
+++ app/containers/Dashboard/WordsPage/components/DictionariesListItem/index.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 // components
-import {Menu, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import {Menu, Icon, Label, Popup} from 'semantic-ui-react';
 
 export default function DictionariesListItem({item}) {
   return (
-    <Menu.Item name={item.title}>
-      <Icon name="home" />
+    <Menu.Item as={Link} to={`/dictionaries/${item.slug}`} name={item.title}>
+      <Popup
+        trigger={<Label>{item.stats.get('wordsCount')}</Label>}
+        content={`${item.stats.get('wordsCount')} words`}
+      />
       {item.title}
     </Menu.Item>
   );
