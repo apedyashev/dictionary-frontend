9495ef4ec98c880d471c28b97df771b39599da34
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-04-21 17:17:40 +0200

    Show diff between wrong and correct answer

--- app/containers/screens/Dashboard/LearnWordsPage/components/TrainWritingCard/index.js
+++ app/containers/screens/Dashboard/LearnWordsPage/components/TrainWritingCard/index.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 import _isEqual from 'lodash/isEqual';
+import * as jsDiff from 'diff';
 // components
-import {Button, Grid, Image} from 'semantic-ui-react';
+import {Button, Grid, Image, Label} from 'semantic-ui-react';
 import {FormatWordDefinitions} from 'components';
 import TrainWritingForm from '../TrainWritingForm';
 // other
@@ -11,6 +12,7 @@ import styles from './index.css';
 
 class TrainWritingCard extends React.PureComponent {
   static propTypes = {};
+  state = {typedPhraseWithDiff: null};
 
   parsePhrase = (phrase) => {
     const punctuationMarksPattern = ',|\\?|\\.|\\-|\\:|\\;|\\!|\\(|\\)|\\[|\\]';
@@ -31,11 +33,24 @@ class TrainWritingCard extends React.PureComponent {
     if (_isEqual(originalPhraseParsed, typedPhraseParsed)) {
       this.props.onNext();
     } else {
-      //
+      const diff = jsDiff.diffChars(word.get('word'), phrase);
+      const typedPhraseWithDiff = [];
+      diff.forEach((part) => {
+        // green for additions, red for deletions
+        // grey for common parts
+        const className = part.added ? styles.added : part.removed ? styles.removed : '';
+        typedPhraseWithDiff.push(<span className={className}>{part.value}</span>);
+        // span = document.createElement('span');
+        // span.style.color = color;
+        // span.appendChild(document.createTextNode(part.value));
+        // fragment.appendChild(span);
+      });
+      this.setState({typedPhraseWithDiff});
     }
   };
 
   render() {
+    const {typedPhraseWithDiff} = this.state;
     const {word} = this.props;
     return (
       <React.Fragment>
@@ -49,7 +64,26 @@ class TrainWritingCard extends React.PureComponent {
             </Grid.Column>
             <Grid.Column width={10}>
               <TrainWritingForm onCheck={this.handleCheck} />
-              <div className={styles.resultMessage}>result message</div>
+              <div className={styles.resultMessage}>
+                {typedPhraseWithDiff && (
+                  <div>
+                    <div className={styles.correctAnswerContainer}>
+                      <div className={styles.header}>Correct answer:</div>
+                      <div>{word.get('word')}</div>
+                    </div>
+                    <div className={styles.details}>
+                      <div className={styles.header}>
+                        You've made a mistake, please check if it's a typo or an error
+                      </div>
+                      <div>
+                        <Label color="red">Missing charachters</Label>
+                        <Label color="green">Redundant charachters</Label>
+                      </div>
+                      <div>{typedPhraseWithDiff}</div>
+                    </div>
+                  </div>
+                )}
+              </div>
               <Button content="Next" />
             </Grid.Column>
           </Grid.Row>
