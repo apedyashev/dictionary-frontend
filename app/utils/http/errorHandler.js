dfb33c9d2c5a2ec50a6b3ee8c48061cb00ba33af
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-05-15 15:49:47 +0200

    Show error responses

--- app/utils/http/errorHandler.js
+++ app/utils/http/errorHandler.js
@@ -1,9 +1,38 @@
 // import {push} from 'react-router-redux';
-import {HTTP_STATUS_NOT_AUTHORIZED, HTTP_STATUS_UNPROCESSABLE_ENTITY} from './httpStatusCodes';
+import {
+  HTTP_STATUS_NOT_AUTHORIZED,
+  HTTP_STATUS_UNPROCESSABLE_ENTITY,
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_FORBIDDEN,
+} from './httpStatusCodes';
 
 // import _ from 'lodash';
 // import {actions as userActions} from 'modules/user';
 import {setError} from 'containers/Notification/actions';
+
+function getErrorMessage(error) {
+  let text = 'Unknown error';
+  if (error instanceof Error) {
+    text = error.toString();
+    if (
+      [HTTP_STATUS_UNPROCESSABLE_ENTITY, HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_FORBIDDEN].indexOf(
+        error.status
+      ) >= 0
+    ) {
+      try {
+        const json = JSON.parse(error.response.text);
+        text = json && json.message;
+      } catch (e) {
+        // the 'text' is already set
+      }
+    }
+  } else if (error && error.json && error.json.details && error.json.details.message) {
+    text = error.json.message;
+  } else if (typeof error === 'string') {
+    text = error;
+  }
+  return text;
+}
 
 export function errorHandler(dispatch) {
   return (err) => {
@@ -12,8 +41,9 @@ export function errorHandler(dispatch) {
       // dispatch(userActions.reset());
       // dispatch(push('/login'));
     } else {
-      console.log('err', err.message);
-      dispatch(setError(err.message));
+      const errorMessage = getErrorMessage(err);
+      console.log('err', errorMessage);
+      dispatch(setError(errorMessage));
     }
 
     // UNPROCESSABLE_ENTITY means 'validation error'. Parse JSON reject the promise with error message
