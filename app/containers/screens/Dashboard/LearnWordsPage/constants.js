ad0db12788e19a0b171daf2f80dec1378518884e
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-06-04 21:45:55 +0200

    Build

--- app/containers/screens/Dashboard/LearnWordsPage/constants.js
+++ app/containers/screens/Dashboard/LearnWordsPage/constants.js
@@ -1,5 +1,5 @@
 export const NUM_OF_OPTIONS_IN_CARD = 6;
-export const NUM_OF_WORDS_TO_LEARN = 2;
+export const NUM_OF_WORDS_TO_LEARN = 7;
 
 export const TRAINING_WORD_TRANSLATION = 'wordTranslation';
 export const TRAINING_TRANSLATION_WORD = 'translationWord';
--- build/sw.js
+++ build/sw.js
@@ -30,7 +30,7 @@ var __wpo = {
       "/ef60a4f6c25ef7f39f2d25a748dbecfe.woff",
       "/cd6c777f1945164224dee082abaea03a.woff2",
       "/9c74e172f87984c48ddf5c8108cabe67.png",
-      "/main.7084967f77fa9a9c5b1a.js",
+      "/main.7d826c11e6b0b672e534.js",
       "/"
     ],
     "additional": [
@@ -39,7 +39,7 @@ var __wpo = {
       "/2.4173590a85ff4306cf4d.chunk.js",
       "/3.429c491b24b109011a42.chunk.js",
       "/4.cf35c6d1826580d9ba3a.chunk.js",
-      "/5.8e7dc5c7efe217462fc3.chunk.js",
+      "/5.d84272a20233357a6d15.chunk.js",
       "/6.79fa7100e6e110ac92b7.chunk.js",
       "/7.dc6619920e286fbdcabd.chunk.js",
       "/8.ee86a0a5849d4b8a0418.chunk.js",
@@ -84,18 +84,18 @@ var __wpo = {
     "c367c1cc4bbe7e578aae1829336a0b1aa2c63759": "/2.4173590a85ff4306cf4d.chunk.js",
     "5cac86f8c9b0883d5bafad5521d7f3a03e8e84b3": "/3.429c491b24b109011a42.chunk.js",
     "b80cdd746c316d0c1bb336cef7d83525518b7b95": "/4.cf35c6d1826580d9ba3a.chunk.js",
-    "ac982db21ef4f422868d1cce379f1090ae7ab927": "/5.8e7dc5c7efe217462fc3.chunk.js",
+    "b54d46ca0410b0fa1214c75300c054ffb835208a": "/5.d84272a20233357a6d15.chunk.js",
     "5cab9ebf72fe71630d00cddea07acf2ae766f26d": "/6.79fa7100e6e110ac92b7.chunk.js",
     "3cf2f917c86b5a86a686b10dafa072d4d344de67": "/7.dc6619920e286fbdcabd.chunk.js",
     "41fdb2e5728c1d6fedeeb94643c29e608ae26527": "/8.ee86a0a5849d4b8a0418.chunk.js",
     "79bb4aa0ba886e6fab2387a923aa5cf33f659de1": "/9.ea6c5df2f84c42ca4957.chunk.js",
     "5a4e8572ef519d2735a535d49f890a61fc57a2bd": "/10.8705a83bbaaebb378c4f.chunk.js",
-    "fdae98731135aa5c0ab178290dec96c59acf3d39": "/main.7084967f77fa9a9c5b1a.js",
-    "4d612105af9c62d9b7ebdb3aea9ca7b6cc07ffd9": "/"
+    "9d9a3ac6786d991f4ead6ef103422f1985a3c23b": "/main.7d826c11e6b0b672e534.js",
+    "e6daf8dfb7ea0e7e46ff799fee22f242c439bfc4": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-6-4 21:12:03",
+  "version": "2018-6-4 21:45:20",
   "name": "webpack-offline",
   "pluginVersion": "5.0.2",
   "relativePaths": false
