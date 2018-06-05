5d443c245310d78e54c4e96c0df343ca8dacd980
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-06-05 17:00:08 +0200

    Home page scroll animation for mobile phones

--- app/containers/screens/HomePage/index.js
+++ app/containers/screens/HomePage/index.js
@@ -5,12 +5,13 @@ import {Helmet} from 'react-helmet';
 import {connect} from 'react-redux';
 import {compose} from 'redux';
 import {createStructuredSelector} from 'reselect';
+import cn from 'classnames';
 //
 import injectReducer from 'utils/injectReducer';
 import {makeSelectIsLoginRoute, makeSelectLocation} from 'containers/App/selectors';
 // components
 import {Link} from 'react-router-dom';
-import {Grid, Ref} from 'semantic-ui-react';
+import {Grid, Ref, Responsive} from 'semantic-ui-react';
 import AuthForms from './components/AuthForms';
 import reducer from './reducer';
 // import saga from './saga';
@@ -19,31 +20,94 @@ import styles from './index.css';
 export class HomePage extends React.PureComponent {
   static propTypes = {
     showLoginForm: PropTypes.bool.isRequired,
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+  state = {
+    currentSlide: ['/register', '/login'].includes(this.props.location.pathname) ? 'auth' : 'video',
   };
   authRef = React.createRef();
+  isMobile = false;
 
-  componentDidUpdate(prevProps) {
-    console.log('====', this.props.location.pathname);
-    if (['/register', '/login'].includes(this.props.location.pathname)) {
-      window.scrollTo(0, this.authContainerNode.offsetTop);
-    }
+  componentDidMount() {
+    window.addEventListener('wheel', this.handleScroll);
   }
+
+  componentDidUnmount() {
+    window.removeEventListener('wheel', this.handleScroll);
+  }
+
+  handleScroll = (e) => {
+    if (this.isMobile) {
+      let {currentSlide} = this.state;
+      if (e.deltaY < 0) {
+        // scrolling up
+        currentSlide = 'video';
+      }
+      if (e.deltaY > 0) {
+        // scrolling down
+        currentSlide = 'auth';
+      }
+
+      this.setState({currentSlide});
+    }
+  };
+
+  handleTouchStart = (evt) => {
+    this.yDown = evt.touches[0].clientY;
+  };
+
+  handleTouchMove = (evt) => {
+    const {yDown} = this;
+    if (!yDown) {
+      return;
+    }
+
+    const yUp = evt.touches[0].clientY;
+
+    const yDiff = yDown - yUp;
+    let {currentSlide} = this.state;
+
+    if (Math.abs(yDiff) > 0) {
+      if (yDiff > 0) {
+        currentSlide = 'auth';
+      } else {
+        currentSlide = 'video';
+      }
+      this.setState({currentSlide});
+    }
+    this.yDown = null;
+  };
 
   handleAuthRef = (node) => {
     this.authContainerNode = node;
-    this.forceUpdate();
+    // this.forceUpdate();
   };
 
   render() {
+    const {currentSlide} = this.state;
     const {showLoginForm} = this.props;
     return (
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        onTouchStart={this.handleTouchStart}
+        onTouchMove={this.handleTouchMove}
+      >
         <Helmet>
           <title>Home Page</title>
           <meta name="description" content="A React.js Boilerplate application homepage" />
         </Helmet>
+        <Responsive {...Responsive.onlyMobile}>{(this.isMobile = true)}</Responsive>
         <Grid>
-          <Grid.Column className={styles.slide} floated="left" computer={11} mobile={16}>
+          <Grid.Column
+            className={cn(styles.slide, styles.video, {
+              [styles.active]: currentSlide === 'video',
+            })}
+            floated="left"
+            computer={11}
+            mobile={16}
+          >
             <p>
               <h3>Change the way you learn words!</h3>
               Learn them in the most efficient way with spaced repetition technique!
@@ -55,10 +119,19 @@ export class HomePage extends React.PureComponent {
               time between subsequent review of previously learned material in order to exploit the
               psychological spacing effect.
             </p>
-            <Link to="/register">Register</Link>
+            <Responsive {...Responsive.onlyMobile}>
+              <Link to="/register">Register</Link>
+            </Responsive>
           </Grid.Column>
           <Ref innerRef={this.handleAuthRef}>
-            <Grid.Column className={styles.slide} floated="right" computer={5} mobile={16}>
+            <Grid.Column
+              className={cn(styles.slide, styles.auth, {
+                [styles.active]: currentSlide === 'auth',
+              })}
+              floated="right"
+              computer={5}
+              mobile={16}
+            >
               <AuthForms showLoginForm={showLoginForm} />
             </Grid.Column>
           </Ref>
