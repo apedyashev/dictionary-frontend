d56046ac93eef6302ba84063c24bd73bc1a8d7df
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2018-08-05 17:04:12 +0200

    Debug

--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,6 @@ export default {
     process.env.NODE_ENV === 'production' ? 'https://dict.apedyashev.com' : 'http://localhost:3001',
   apiBaseUrl:
     process.env.NODE_ENV === 'production'
-      ? 'https://dict-api.apedyashev.com'
+      ? 'https://dict-api1.apedyashev.com'
       : 'http://localhost:3001',
 };
